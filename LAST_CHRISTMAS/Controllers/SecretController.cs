ad0efbb1328300ddae075667f4a10d1d2b93b64a
Łukasz Górecki <ukasz Grecki>
2016-12-08 06:32:59 +0100

    Stage 2

---
+++ LAST_CHRISTMAS/Controllers/SecretController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LAST_CHRISTMAS.Controllers
+{
+    public class SecretController : Controller
+    {
+        class RiddleResponse
+        {
+            public string BandStatus { get; set; }
+            public string SantaStatus { get; set; }
+            public string GiftStatus { get; set; }
+            public string FinalLink { get; set; }
+        }
+
+
+        // GET: Secret
+        
+        public ActionResult Status(string bandCode, string santaCode, string giftCode)
+        {
+            const string ERROR_CODE = "ERROR";
+            const string OK_CODE = "OK";
+
+            var response = new RiddleResponse()
+            {
+                BandStatus = ERROR_CODE,
+                GiftStatus = ERROR_CODE,
+                SantaStatus = ERROR_CODE
+            };
+
+            if(bandCode == "2304")
+            {
+                response.BandStatus = OK_CODE;
+            }
+
+            if(!String.IsNullOrEmpty(giftCode) && giftCode.ToLower() == "csiwykop")
+            {
+                response.GiftStatus = OK_CODE;
+            }
+
+            if(!String.IsNullOrEmpty(santaCode) && santaCode.ToLower() == "wrocław")
+            {
+                response.SantaStatus = OK_CODE;
+            }
+
+            if(response.BandStatus == OK_CODE && response.GiftStatus == OK_CODE && response.SantaStatus == OK_CODE)
+            {
+                response.FinalLink = Url.Action("tjlcdgr", "Home");
+            }
+
+
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+       
+    }
+}
--- LAST_CHRISTMAS/Scripts/all_members.js
+++ LAST_CHRISTMAS/Scripts/all_members.js
@@ -1,7 +1,6 @@
 ﻿jQuery(function () {
     var code = [];
 
-
     jQuery('.band-element').on('click', function (e) {
         var currentState = jQuery(this).attr('data-state');
         var value = jQuery(this).attr('data-id');
@@ -15,7 +14,60 @@
             jQuery(this).attr('data-state', 'unchecked');
         }
 
-        jQuery('#codeValue').text(code.join(''));
-        console.log(code);
+        var computedCode = code.join('');
+        jQuery('#bandCodeValue').text(computedCode);
+        checkCodes();        
     });
+
+    jQuery('#giftCodeValue').on('change', function () {
+        checkCodes();
+    });
+
+    jQuery('#santaCodeValue').on('change', function () {
+        checkCodes();
+    });
+
+    function checkCodes() {
+        var bandValue = jQuery('#bandCodeValue').text();
+        var giftValue = jQuery('#giftCodeValue').val();
+        var santaValue = jQuery('#santaCodeValue').val();
+
+        jQuery.ajax(
+            '/Secret/Status', {
+                data: {
+                    bandCode: bandValue, 
+                    santaCode: santaValue,
+                    giftCode: giftValue
+                }
+        }).success(function (data) {
+            console.log(data);
+            if (data.BandStatus == "OK") {
+                jQuery('#bandLock').removeClass('glyphicon-lock locked').addClass('glyphicon-ok unlocked');
+            }
+            else {
+                jQuery('#bandLock').removeClass('glyphicon-ok unlocked').addClass('glyphicon-lock locked');
+            }
+
+            if (data.GiftStatus == "OK") {
+                jQuery('#giftLock').removeClass('glyphicon-lock locked').addClass('glyphicon-ok unlocked');
+            }
+            else {
+                jQuery('#giftLock').removeClass('glyphicon-ok unlocked').addClass('glyphicon-lock locked');
+            }
+
+            if (data.SantaStatus == "OK") {
+                jQuery('#santaLock').removeClass('glyphicon-lock locked').addClass('glyphicon-ok unlocked');
+            }
+            else {
+                jQuery('#santaLock').removeClass('glyphicon-ok unlocked').addClass('glyphicon-lock locked');
+            }
+
+            if (data.SantaStatus == "OK" && data.GiftStatus == "OK" && data.BandStatus == "OK") {
+                window.location.href = data.FinalLink;
+            }
+
+
+        });
+    }
+
 });
